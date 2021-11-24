c567b4e9ea52ddf758c028ed2c8b7702e86d5470
Tushar Kashyap <65089058+tusharkashyap63@users.noreply.github.com>
2021-11-25 01:17:54 +0530

    frontend authentication form

--- src/App.js
+++ src/App.js
@@ -5,10 +5,17 @@ import Cookies from 'universal-cookie';
 import ChanelListContainer from './components/ChanelListContainer';
 import ChanelContainer from './components/ChanelContainer';
 import './App.css';
+import Auth from './components/Auth';
+
 const apiKey = 'dw9thsfxkaws';
 
 const client = StreamChat.getInstance(apiKey);
+
+const authToken = false;
+
 function App() {
+  if (!authToken) return <Auth />;
+
   return (
     <div className='app__wrapper'>
       <Chat client={client} theme='team light'>
